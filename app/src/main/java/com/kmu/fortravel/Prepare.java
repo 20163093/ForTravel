dc13d382b84e710626f921468d3dda8b410aa6bc
20163093 <kimsung97@naver.com>
2018-12-12 17:22:37 +0900

    Prepare checkbox add clear

--- app/src/main/java/com/kmu/fortravel/Prepare.java
+++ app/src/main/java/com/kmu/fortravel/Prepare.java
@@ -1,13 +1,13 @@
 package com.kmu.fortravel;
 
-import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.EditText;
-import android.widget.ImageButton;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Prepare extends AppCompatActivity {
 
@@ -16,22 +16,71 @@ public class Prepare extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prepare);
 
-        final EditText editText = (EditText) findViewById(R.id.editText);
-        ImageButton imageButton = (ImageButton) findViewById(R.id.imageButton);
-        final CheckBox checkBox = (CheckBox) findViewById(R.id.checkBox);
+        // 빈 데이터 리스트 생성.
+        final ArrayList<String> items = new ArrayList<String>() ;
+        // ArrayAdapter 생성. 아이템 View를 선택(single choice)가능하도록 만듦.
+        final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, items) ;
 
-        imageButton.setOnClickListener(new View.OnClickListener()   {
-            @Override
-            public void onClick (View v) {
-                checkBox.setText(editText.getText());
+        // listview 생성 및 adapter 지정.
+        final ListView listview = (ListView) findViewById(R.id.listview1) ;
+        listview.setAdapter(adapter) ;
 
+        Button addButton = (Button)findViewById(R.id.add) ;
+        addButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                int count;
+                count = adapter.getCount();
+
+                // 아이템 추가.
+                items.add("LIST" + Integer.toString(count + 1));
+
+                // listview 갱신
+                adapter.notifyDataSetChanged();
             }
-        });
+        }) ;
+
+        Button modifyButton = (Button)findViewById(R.id.modify) ;
+        modifyButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                int count, checked ;
+                count = adapter.getCount() ;
+
+                if (count > 0) {
+                    // 현재 선택된 아이템의 position 획득.
+                    checked = listview.getCheckedItemPosition();
+                    if (checked > -1 && checked < count) {
+                        // 아이템 수정
+                        items.set(checked, Integer.toString(checked+1) + "번 아이템 수정") ;
+
+                        // listview 갱신
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+            }
+        }) ;
+
+        Button deleteButton = (Button)findViewById(R.id.delete) ;
+        deleteButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                int count, checked ;
+                count = adapter.getCount() ;
+
+                if (count > 0) {
+                    // 현재 선택된 아이템의 position 획득.
+                    checked = listview.getCheckedItemPosition();
+
+                    if (checked > -1 && checked < count) {
+                        // 아이템 삭제
+                        items.remove(checked) ;
+
+                        // listview 선택 초기화.
+                        listview.clearChoices();
+
+                        // listview 갱신.
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+            }
+        }) ;
     }
-
-
-
-
-
-
 }
